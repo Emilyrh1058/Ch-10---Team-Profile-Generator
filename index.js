532591173bfc2f8250237a1bab1b5862313ff13a
Emily Salinas <emilyrh1058@gmail.com>
2021-04-25 13:08:46 -0500

    need to add section for employee add

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 // CREATE THE PROMPTS FOR MANAGER HERE. USE INQUIRER
+function managerPrompt() {
 inquirer
   .prompt([
     {
@@ -29,20 +30,65 @@ inquirer
       name: "officeNumber",
       message: "What is their office number?",
     },
-    // {             // SHOULD I USE "ADD TEAM MEMBER" HERE?
-    //   type: "",
-    //   name: "",
-    //   message: "",
-    // },
+    {             
+      type: "confirm",
+      name: "addEmployee",
+      message: "Would you like to add an employee?",
+      validate: (addEmployee) => {
+        if (addEmployee) {return true;} 
+        else {return ""}
+      }
+    },
+    {
+      type: "input",
+      name: "employee",
+      message: "Please list the names of the contributors.",
+      when: ({ confirmContributors }) => {
+          if (confirmContributors) {return true;} 
+          else {return false;}
+      }
+  },
+
+
   ])
 
   .then(function (response) {
     console.log("Manager section is complete."); // CONSOLE OR RETURN?
   });
+};
 
+function employeePrompt() {
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Enter employee's name:",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter ID number:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter the employee's email:",
+    },
+    {
+      type: "list",
+      name: "position",
+      message: "What is their current position?",
+      choices: ["Engineer", "Intern"],
+    },
+  ])
 
-
-
-
-
-
+  .then(function (response) {
+    employee = response;
+    if (response.position === "Intern") {
+      internPrompt();
+    } else {
+      engineerPrompt();
+    }
+  })
+};
